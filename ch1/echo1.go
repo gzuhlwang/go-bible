bc5152a9ac38aed0c63006c12bb5771b1f880158
gzuhlwang <2277581700@qq.com>
2018-05-11 16:50:07 +0800

    add comment

--- ch1/echo1.go
+++ ch1/echo1.go
@@ -9,7 +9,7 @@ func main(){
 	var s,sep string
 	fmt.Println("os.Args:",os.Args)  //输出整个os.Args
 	fmt.Println(os.Args[0])            //os.Args中第一个参数是该程序的路径
-	for i:=1;i<len(os.Args);i++{
+	for i:=1;i<len(os.Args);i++{       //注意i从1开始，因为我们只需要打印从命令行传进来的参数，索引为0的不符合要求
 		s+=sep+os.Args[i]
 		sep=" "
 	}
