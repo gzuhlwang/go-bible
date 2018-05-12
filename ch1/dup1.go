9246545c032637306132c621bc0dc65f22d37390
gzuhlwang <2277581700@qq.com>
2018-05-12 12:24:32 +0800

    first version of dup program

---
+++ ch1/dup1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main(){
+	counts:=make(map[string]int)
+	input:=bufio.NewScanner(os.Stdin)
+
+	for input.Scan(){   //quit by typing Ctrl+D
+		counts[input.Text()]++
+	}
+	//filter duplicate line and its content
+	for line,n := range counts{
+		if n>1{
+			fmt.Printf("The num of dup line is %d and its corresponding content are %s\n",n,line)
+		}
+	}
+	fmt.Println("The result of input:",counts)
+}
